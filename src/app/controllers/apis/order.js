b0f1267609043cdafa5f4f4a4709889a51b2deda
Cao Anh Nhất <anhnhata8cool@gmail.com>
2020-07-30 20:56:00 +0700

    fix repo

--- src/app/controllers/apis/order.js
+++ src/app/controllers/apis/order.js
@@ -11,11 +11,8 @@ exports.order = CatchAsync(async (req, res) => {
   const body = req.body;
 
   const idsPrd = body.items.map((item) => item.prd_id);
-  console.log("exports.order -> body.items", body.items);
-  console.log("exports.order -> idsPrd", idsPrd);
 
   const products = await ProductModel.find({ _id: { $in: idsPrd } }).lean();
-  console.log("exports.order -> products", products);
 
   const order = {
     fullName: body.name,
@@ -27,12 +24,15 @@ exports.order = CatchAsync(async (req, res) => {
   const items = [];
   let totalPrice = 0;
 
-  for (let item of body.items) {
-    const prd = _.find(products, { _id: item.prd_id });
-    if (prd) {
-      item.price = prd.price;
-      totalPrice += item.qty * prd.price;
-      items.push(item);
+  for (let prd of products) {
+    const cart = _.find(body.items, {
+      prd_id: prd._id.toString(),
+    });
+
+    if (cart) {
+      cart.price = prd.price;
+      totalPrice += cart.qty * prd.price;
+      items.push(cart);
     }
   }
 
@@ -42,7 +42,7 @@ exports.order = CatchAsync(async (req, res) => {
   await new OrderModel(order).save();
 
   const html = await renderHtml(req, "mail", {
-    products,
+    products: products,
     cart: body.items,
     name: body.name,
     phone: body.phone,
@@ -53,14 +53,14 @@ exports.order = CatchAsync(async (req, res) => {
     formatPrice,
   });
 
-  await transporter.sendMail({
+  transporter.sendMail({
     from: '"Vietpro Shop" <mail@gmail.com>',
     to: body.email,
     subject: "Thong tin don hang",
     html: html,
   });
 
-  res.status(201).json({
+  return res.status(201).json({
     status: "success",
     message: "Create order succesfully",
   });
